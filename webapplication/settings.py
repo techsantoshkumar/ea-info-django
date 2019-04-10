f765e5dd37a36550c4339fdef7516a24c178ffb3
santosh <tech.santoshkumar@gmail.com>
2019-04-10 11:04:02 +0530

    git commit

--- webapplication/settings.py
+++ webapplication/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+#for heroku deploying add this static
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'

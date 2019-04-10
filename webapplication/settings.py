b279f05850afbd4de7f659013972fa4c55992a69
santosh <tech.santoshkumar@gmail.com>
2019-04-10 10:31:14 +0530

    v-1.1 modified

--- webapplication/settings.py
+++ webapplication/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'webapplication.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES={
-    'default':{
-        'ENGINE':'django.db.backends.mysql',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'NAME':'ea_info_biz_django',
-        'PORT':3306
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES={
+#     'default':{
+#         'ENGINE':'django.db.backends.mysql',
+#         'USER':'root',
+#         'PASSWORD':'',
+#         'HOST':'localhost',
+#         'NAME':'ea_info_biz_django',
+#         'PORT':3306
+#
+#     }
+# }
 
 
 # Password validation
